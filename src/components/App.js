7f9a5188237e93398325788d523c48194cfead0f
neema <neemabridgetteodire2@gmail.com>
2024-05-05 00:32:20 +0300

    use-effect-lab

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ function App() {
   const [currentQuestionId, setCurrentQuestion] = useState(1);
   const [score, setScore] = useState(0);
   const currentQuestion = questions.find((q) => q.id === currentQuestionId);
-
+  
   function handleQuestionAnswered(correct) {
     if (currentQuestionId < questions.length) {
       setCurrentQuestion((currentQuestionId) => currentQuestionId + 1);
